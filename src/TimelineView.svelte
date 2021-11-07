f699a46f328abf52204e7df8028b1394f50e7833
mard-one <mardon.mashrabov@gmail.com>
2021-11-08 01:20:07 +0500

    issue with loading timeline fixed

--- src/TimelineView.svelte
+++ src/TimelineView.svelte
@@ -1,55 +1,66 @@
 <script>
-  import { onMount } from "svelte";
+  import { listOfIdeas, timelineClass } from "./stores.js";
+  let timeline, ideas;
 
-  import { listOfIdeas } from "./stores.js";
-  // console.log("$listOfIdeas", $listOfIdeas);
-
-  const init = () => {
-    listOfIdeas.subscribe((ideas) => {
-      // console.log("subscribe ideas", ideas);
-      if (window.TL && ideas) {
-        let timeline = new window.TL.Timeline(
-          "timeline-embed",
-          {
-            scale: "cosmological",
-            title: {
-              media: {
-                caption: "",
-                credit: "",
-                url: "",
-                thumbnail: "",
-              },
-              text: {
-                headline: "History of ideas",
-                text: "The history of ideas that is a collection of everything that humankind has thought, invented, created, considered, and perfected from the beginning of civilization to these days",
-              },
-            },
-            events: ideas,
+  timelineClass.subscribe((tl) => {
+    // console.log("subscribe tl", tl);
+    timeline = tl;
+  });
+  listOfIdeas.subscribe((i) => {
+    // console.log("subscribe ideas", ideas);
+    ideas = i;
+  });
+  // console.log("timeline", timeline);
+  // console.log("ideas", ideas);
+  $: if (timeline && ideas.length) {
+    // console.log("timeline init");
+    let timelineRef = new timeline.Timeline(
+      "timeline-embed",
+      {
+        scale: "cosmological",
+        title: {
+          media: {
+            caption: "",
+            credit: "",
+            url: "",
+            thumbnail: "",
           },
-          {
-            optimal_tick_width: 50,
-            hash_bookmark: true,
-            duration: 500,
-            debug: true,
-          }
-        );
-        // console.log('timeline', timeline);
-        timeline.on("ready", (e) => {
-          // console.log("now ready");
-          for (const idea of e.target.config.events) {
-            const container = document.getElementById(idea.unique_id);
-            // console.log("container", container);
-            const link = document.createElement("a");
-            link.innerText = "Learn more...";
-            link.setAttribute("target", "_self");
-            link.setAttribute("href", `/ideas/${idea.articleFileName}`);
-            container.querySelector(".tl-text-content").appendChild(link);
-          }
-        });
+          text: {
+            headline: "History of ideas",
+            text: "The history of ideas that is a collection of everything that humankind has thought, invented, created, considered, and perfected from the beginning of civilization to these days",
+          },
+        },
+        events: ideas,
+      },
+      {
+        optimal_tick_width: 50,
+        hash_bookmark: true,
+        duration: 500,
+        debug: true,
+        font: 'bitter-raleway'
+        // font: 'georgia-helvetica'
+        // font: 'lustria-lato'
+        // font: 'opensans-gentiumbook'
+        // font: 'rufina-sintony'
+      }
+    );
+    // console.log('timeline', timeline);
+    timelineRef.on("ready", (e) => {
+      // console.log("now ready");
+      for (const idea of e.target.config.events) {
+        const container = document.getElementById(idea.unique_id);
+        // console.log("container", container);
+        const link = document.createElement("a");
+        link.innerText = "Learn more...";
+        link.setAttribute("target", "_self");
+        link.setAttribute("href", `/ideas/${idea.articleFileName}`);
+        container.querySelector(".tl-text-content").appendChild(link);
       }
     });
-  };
-  onMount(init);
+  }
 </script>
 
-<div id="timeline-embed" style="height: calc(100vh - 70px); overflow: visible" />
+<div
+  id="timeline-embed"
+  style="height: calc(100vh - 70px); overflow: visible"
+/>
